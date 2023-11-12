a8335629b97dcdfbaf426fa1b10cc11b5cfbe626
ilyademo <82061894+ilyademo@users.noreply.github.com>
2023-11-12 13:10:20 +0300

    Delete Solve_file.h

--- Solve_file.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Initialize_file.h"
-
-void SolveEq(VD& u, const VD& mu, const VD& T, double dy, double gradP, double T0, double dT, bool m = true);
-
-void CalcAverageVel(const VD& U, const VD& y, double dy, double H, double Re, double rho);
-
-void CalcViscDeviation(const VD& mu);
