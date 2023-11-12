4c8284020a245178ddf62cb74f4907f6238484b4
ilyademo <82061894+ilyademo@users.noreply.github.com>
2023-11-12 13:11:10 +0300

    Delete Initialize_file.h

--- Initialize_file.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "in_out_file.h"
-
-
-double mu_T(double T);
-
-void CreateMesh(VD& y_nodes, VD& y_cells, int n, double H, double& dy);
-
-void InitializeFields(VD& T, VD& U, VD& mu, VD& y, double T0, double dT);
